31ad642057e548ea9a8cb967b7ae87776dddda24
Zachary Mullins <zmullins@uwo.ca>
2018-04-08 04:05:45 -0400

    Database server will now be the method of maintaining all business logic model objects

---
+++ src/databaseServer/databaseServer.java
@@ -0,0 +1,47 @@
+package databaseServer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import offerings.CourseOffering;
+import offerings.ICourseOffering;
+import offerings.OfferingFactory;
+import registrar.ModelRegister;
+import systemUsers.StudentModel;
+
+public class databaseServer {
+	public void load() {
+//	Create an instance of an OfferingFactory
+	OfferingFactory factory = new OfferingFactory();
+	BufferedReader br = new BufferedReader(new FileReader(new File("note_1.txt")));
+//	Use the factory to populate as many instances of courses as many files we've got.
+	CourseOffering	courseOffering = factory.createCourseOffering(br);
+	br.close();
+//	Loading 1 file at a time
+	br = new BufferedReader(new FileReader(new File("note_2.txt")));
+//	here we have only two files
+	courseOffering = factory.createCourseOffering(br);
+	br.close();
+//	code to perform sanity checking of all our code
+//	by printing all of the data that we've loaded
+	for(CourseOffering course : ModelRegister.getInstance().getAllCourses()){
+		System.out.println("ID : " + course.getCourseID() + "\nCourse name : " + course.getCourseName() + "\nSemester : " + 
+		course.getSemester());
+		System.out.println("Students allowed to enroll\n");
+		for(StudentModel student : course.getStudentsAllowedToEnroll()){
+			System.out.println("Student name : " + student.getName() + "\nStudent surname : " + student.getSurname() + 
+					"\nStudent ID : " + student.getID() + "\nStudent EvaluationType : " + 
+					student.getEvaluationEntities().get(course) + "\n\n");
+		}
+		
+		for(StudentModel student : course.getStudentsAllowedToEnroll()){
+			for(ICourseOffering course2 : student.getCoursesAllowed())
+			System.out.println(student.getName() + "\t\t -> " + course2.getCourseName());
+		}
+	}
+	
+	}
+	
+}
+}
